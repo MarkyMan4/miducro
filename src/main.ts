7464e44a290def2234271d44123ac26cb097efb5
Markus Hutnik <markus.hutnik@gmail.com>
2025-10-18 00:00:28 -0500

    drawing sprites

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 'use strict';
 
 import * as LJS from 'littlejsengine';
-import { vec2, rgb } from 'littlejsengine';
+import { vec2, hsl, tile } from 'littlejsengine';
 
+const backgroundColor = hsl(1.27, 0.51, 0.17);
+let levelSize;
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameInit()
 {
     // called once after the engine starts up
     // setup the game
+    LJS.setCanvasFixedSize(vec2(1920, 1080));
+    LJS.setCanvasClearColor(backgroundColor);
+
+    // levelSize = vec2(20, 20);
+    // LJS.setCameraPos(levelSize.scale(0.5))
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -30,6 +37,8 @@ function gameRender()
 {
     // called before objects are rendered
     // draw any background effects that appear behind objects
+    LJS.drawTile(LJS.cameraPos, vec2(3), tile(0, 90))
+    LJS.drawTile(vec2(LJS.cameraPos.x + 2, LJS.cameraPos.y), vec2(3), tile(3, 90))
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -37,7 +46,7 @@ function gameRenderPost()
 {
     // called after objects are rendered
     // draw effects or hud that appear above all objects
-    LJS.drawTextScreen('Hello World!', LJS.mainCanvasSize.scale(.5), 80);
+    // LJS.drawTextScreen('Hello World!', LJS.mainCanvasSize.scale(.5), 80);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
